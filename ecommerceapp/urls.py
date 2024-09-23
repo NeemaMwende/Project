ef38a32f8bf6807c25cfc69598a4f6d45ab311e9
Neema <neemamwende009@gmail.com>
2024-09-24 00:27:00 +0300

    updates

--- ecommerceapp/urls.py
+++ ecommerceapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('category/', views.categoryView.as_view(),name="category")
+    path('', views.home, name="home"),
+    path("category/<slug:val>", views.CategoryView.as_view(), name="category"),
 ]
--- ecommerceapp/views.py
+++ ecommerceapp/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from urllib import request
 from django.views import View
 
 # Create your views here.
 def home(request):
     return render(request, "app/home.html")
 
-class categoryView(View):
+class CategoryView(View):
     def get(self, request, val):
-        return render(request, "app/category.html",locals())
+        return render(request, "app/category.html", locals())
