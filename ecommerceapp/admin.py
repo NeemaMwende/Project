6f6890427764ede3ed3b2cc548295d9c53c1a591
Neema <neemamwende009@gmail.com>
2024-09-23 23:30:55 +0300

    products creation

--- ecommerceapp/admin.py
+++ ecommerceapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . models import Product
 
 # Register your models here.
+@admin.register(Product)
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display = ['id','title','discounted_price','category','product_image']
+    # prepopulated_fields = {'slug': ('name',)}
+    # list_filter = ('category', 'is_available')
+    # list_editable = ('is_available',)
