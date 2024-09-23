31617629be31e53740365c6922f79bee745dee8c
Neema <neemamwende009@gmail.com>
2024-09-24 01:40:18 +0300

    updates

--- ecommerceapp/urls.py
+++ ecommerceapp/urls.py
@@ -1,5 +1,8 @@
+# urls.py
 from django.urls import path
 from . import views
+
+app_name = 'app'  # Ensure your app is namespaced
 
 urlpatterns = [
     path('', views.home, name="home"),
--- ecommerceapp/views.py
+++ ecommerceapp/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Product, CATEGORY_CHOICES  # Add CATEGORY_CHOICES here
 
-# Create your views here.
+
+
 def home(request):
     return render(request, "app/home.html")
 
 class CategoryView(View):
     def get(self, request, val):
-        return render(request, "app/category.html", locals())
+        products = Product.objects.filter(category=val)
+        categories = dict(CATEGORY_CHOICES)  # Now this should work
+        context = {
+            'products': products,
+            'categories': categories,
+        }
+        return render(request, "app/category.html", context)
