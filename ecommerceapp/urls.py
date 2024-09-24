4d61cb759cc54519791dd83ee7690b7726c1b61c
Neema <neemamwende009@gmail.com>
2024-09-24 18:45:22 +0300

    updates

--- ecommerceapp/urls.py
+++ ecommerceapp/urls.py
@@ -7,4 +7,4 @@ app_name = 'app'  # Ensure your app is namespaced
 urlpatterns = [
     path('', views.home, name="home"),
     path("category/<slug:val>", views.CategoryView.as_view(), name="category"),
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
