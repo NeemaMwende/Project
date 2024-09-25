2eeb2b31f92df036206fbebb81eedbc2ab9ee9db
Neema <neemamwende009@gmail.com>
2024-09-25 15:01:39 +0300

    updates

--- ecommerceapp/urls.py
+++ ecommerceapp/urls.py
@@ -1,10 +1,16 @@
 # urls.py
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'app'  # Ensure your app is namespaced
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('about/', views.home, name="home"),
+    path('contact/', views.home, name="home"),
     path("category/<slug:val>", views.CategoryView.as_view(), name="category"),
+    path("category-title/<val>", views.CategoryTitle.as_view(), name="category-title"),
+    path("product-detail/<int:pk>", views.ProductDetail.as_view(), name="product-detail"),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ecommerceapp/views.py
+++ ecommerceapp/views.py
@@ -7,6 +7,12 @@ from .models import Product, CATEGORY_CHOICES  # Add CATEGORY_CHOICES here
 def home(request):
     return render(request, "app/home.html")
 
+def about(request):
+    return render(request, "app/about.html")
+
+def home(request):
+    return render(request, "app/contact.html")
+
 class CategoryView(View):
     def get(self, request, val):
         products = Product.objects.filter(category=val)
@@ -16,3 +22,17 @@ class CategoryView(View):
             'categories': categories,
         }
         return render(request, "app/category.html", context)
+    
+class CategoryTitle(View):
+    def get(self, request, val):
+        product = Product.objects.filter(title=val)
+        title = Product.objects.filter(category=product[0].category).values('title').annotate(total=Count(''))
+        return render(request, "app/category_title.html", context)
+    
+class ProductDetail(View):
+    def get(self, request, pk):
+        product = Product.objects.get(pk=pk)
+        context = {
+            'product': product,
+        }
+        return render(request, "app/product_detail.html", context)
