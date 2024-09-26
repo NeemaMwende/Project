63c88eff4dc3309708645681903aee602b7031dd
Neema <neemamwende009@gmail.com>
2024-09-26 11:50:44 +0300

    profile section updates

--- ecommerceapp/views.py
+++ ecommerceapp/views.py
@@ -64,6 +64,24 @@ class ProfileView(View):
         form = CustomerProfileForm()
         return render(request, "app/profile.html", locals())
     def post(self, request):
+        form = CustomerProfileForm(request.POST)
+        if form.is_valid():
+            user = request.user
+            name = form.cleaned_data['name']
+            locality = form.cleaned_data['locality']
+            city = form.cleaned_data['city']
+            mobile = form.cleaned_data['mobile']
+            state = form.cleaned_data['state']
+            zipcode = form.cleaned_data['zipcode']
+            
+            reg = Customer(user=user, name=name, locality=locality,mobile=mobile,city=city,state=state,zipcode=zipcode)
+            reg.save() 
+            messages.success(request,"Congratulations! Profile Saved Successfully")
+        else:
+            messages.warning(request, "Invalid Input Data")
         return render(request, "app/profile.html", locals())
     
+def address(request):
+    add = Customer.objects.filter(user=request.user)
+    return render(request, 'app/address.html',locals())
     
