617b96b5b7fa6a07aa18b7deeccaca524ff9c043
Neema <neemamwende009@gmail.com>
2024-09-28 13:01:50 +0300

    updates

--- ecommerceproject/settings.py
+++ ecommerceproject/settings.py
@@ -13,12 +13,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-import environ
+# import environ
 
-import environ
+# import environ
 
-env = environ.Env()
-environ.Env.read_env()
+# env = environ.Env()
+# environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
