a8254efb5ce832005624906395f7cdc6f6c00380
Neema <neemamwende009@gmail.com>
2024-09-28 12:48:28 +0300

    updates

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
 web: gunicorn ecommerceproject.wsgi
+web: gunicorn ecommerceproject.wsgi --log-file -
+
--- ecommerceproject/settings.py
+++ ecommerceproject/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,6 +83,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ecommerceproject.wsgi.application'
 
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
